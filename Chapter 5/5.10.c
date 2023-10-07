7c443bcb041b72594acc8e316fb203eb6d48cc28
HossamAhmedAli <56847513+HossamAhmedAli@users.noreply.github.com>
2023-10-07 12:39:43 +0300

    Add files via upload

--- Chapter 5/5.10.c
+++ Chapter 5/5.10.c
@@ -3,15 +3,15 @@
 double floor_round(double input);
  void main()
  {
-     double number; // number from the user
+     double number; // number from the user .. local variable >> init=garabage....
     while(1)
     {
      printf("Enter the number: ");
      scanf("%lf",&number); // read number
-     printf("The original number is ( %.2lf ).. The rounded number is ( %d )\n",number,(int)floor_round(number));// (int) is to get the integer value of the return double
+     printf("The original number is ( %.2lf ).. The rounded number is ( %d )\n",number,(int)floor_round(number));// (int) is to get and show the integer value of the return double only
     }
  }
 double floor_round(double input)
 {
-    return floor(input+.5);
+    return floor((input+.5);
 }
